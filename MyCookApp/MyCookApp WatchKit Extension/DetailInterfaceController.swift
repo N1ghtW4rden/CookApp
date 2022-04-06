ec60b9f6c7296028c5a2402284684c7d71dea674
N1ghtW4rden <90888516+N1ghtW4rden@users.noreply.github.com>
2022-04-06 11:54:07 +0900

    Add files via upload

---
+++ MyCookApp/MyCookApp WatchKit Extension/DetailInterfaceController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailInterfaceController.swift
+//  MyCookApp WatchKit Extension
+//
+//  Created by Горохов Никита Исип20 on 06.04.2022.
+//
+
+import WatchKit
+import Foundation
+
+class DetailInterfaceController: NSObject {
+    
+    @IBOutlet var iconLabel: WKInterfaceLabel!
+    @IBOutlet var nameLabel: WKInterfaceLabel!
+    @IBOutlet var recipeLabel: WKInterfaceLabel!
+    @IBOutlet var recipeTitleLabel: WKInterfaceLabel!
+    
+    
+    
+}
